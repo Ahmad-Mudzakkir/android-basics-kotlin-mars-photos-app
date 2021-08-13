ea706abd643bacced0e066b8b0a59a3c5e2e2f66
Ahmad-Mudzakkir <ahmadmudzakkir@gmail.com>
2021-08-14 05:16:12 +0800

    Add Image Display

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -19,29 +19,46 @@ package com.example.android.marsphotos.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
+import kotlinx.coroutines.launch
+import java.util.*
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
+  // The internal MutableLiveData that stores the status of the most recent request
+  private val _status = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status
-    val status: LiveData<String> = _status
-    /**
-     * Call getMarsPhotos() on init so we can display status immediately.
-     */
-    init {
-        getMarsPhotos()
-    }
+  // The external immutable LiveData for the request status
+  val status: LiveData<String> = _status
 
-    /**
-     * Gets Mars photos information from the Mars API Retrofit service and updates the
-     * [MarsPhoto] [List] [LiveData].
-     */
-    private fun getMarsPhotos() {
-        _status.value = "Set the Mars API status response here!"
+  private val _photos = MutableLiveData<List<MarsPhoto>>()
+  val photos: MutableLiveData<List<MarsPhoto>> = _photos
+
+
+  /**
+   * Call getMarsPhotos() on init so we can display status immediately.
+   */
+  init {
+    getMarsPhotos()
+  }
+
+  /**
+   * Gets Mars photos information from the Mars API Retrofit service and updates the
+   * [MarsPhoto] [List] [LiveData].
+   */
+  private fun getMarsPhotos() {
+    viewModelScope.launch {
+      try {
+        _photos.value = MarsApi.retrofitService.getPhotos()
+        _status.value = "Success: Mars properties retrieved"
+      } catch (e: Exception) {
+        _status.value = "Failure: ${e.message}"
+      }
     }
+  }
 }
